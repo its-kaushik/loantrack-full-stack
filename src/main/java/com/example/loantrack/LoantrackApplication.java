500669b5fcd2b2a6e025e17b8392f1526ac93f3e
Mayank Kaushik <mkaushik0033@gmail.com>
2025-02-22 19:44:45 +0530

    Init Commit

---
+++ src/main/java/com/example/loantrack/LoantrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.loantrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoantrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoantrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loantrack/LoantrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loantrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoantrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
