30339f0f455cd22f1d7aaf9d5c2e417b59c1135c
Mayank Kaushik <mkaushik0033@gmail.com>
2025-04-07 15:33:49 +0530

    ADD: Added get Borrower By ID Endpoint

--- api/src/main/java/com/example/loantrack/borrower/BorrowerRepository.java
+++ api/src/main/java/com/example/loantrack/borrower/BorrowerRepository.java
@@ -2,6 +2,8 @@ package com.example.loantrack.borrower;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BorrowerRepository extends JpaRepository<Borrower, Long> {
+import java.util.Optional;
 
+public interface BorrowerRepository extends JpaRepository<Borrower, Long> {
+    Optional<Borrower> getBorrowerById(Long id);
 }
